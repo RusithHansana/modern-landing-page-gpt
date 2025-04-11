0f1e5df311c0da0afa4e6eea355e4f99ba7764dd
RusithHansana <97432521+RusithHansana@users.noreply.github.com>
2025-04-11 22:14:03 +0530

    feat: add Header component with responsive design and styling

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Navbar from './components/navbar/Navbar'
 import Blog from './containers/Blog'
 import Feature from './containers/Feature'
 import Footer from './containers/Footer'
-import Header from './containers/Header'
+import Header from './containers/header/Header'
 import Possibility from './containers/Possibility'
 import WhatGPT from './containers/WhatGPT'
 
---
+++ src/containers/header/Header.jsx
@@ -0,0 +1,31 @@
+import ai from "../../assets/ai.png"
+import people from "../../assets/people.png"
+
+import "./header.css"
+
+const Header = () => {
+    return (
+        <header className="gpt__header section__padding" id="home">
+            <div className="gpt__header-content">
+                <h1 className="gradient__text">Let's Build something amazing with GPT OpenAI</h1>
+                <p>Try out amazing features that comes with GPT to create anything of your imagination.</p>
+
+                <div className="gpt__header-content__input">
+                    <input type="email" placeholder="Your Email Address" />
+                    <button type="button">Get Started</button>
+                </div>
+
+                <div className="gpt__header-content__people">
+                    <img src={people} alt="people" />
+                    <p>1,600 people requested access in the last 24 hours</p>
+                </div>
+            </div>
+
+            <div className="gpt__header-image">
+                <img src={ai} alt="ai" />
+            </div>
+        </header>
+    )
+}
+
+export default Header
